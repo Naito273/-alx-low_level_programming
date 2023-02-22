ef62a94d9710846196234ebb7ca3d8a0f6c05926
Naito273 <naitore2kimathi@gmail.com>
2023-02-23 02:53:30 +0300

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,21 +2,16 @@
 
 /**
  * print_alphabet - Make the alphabet
- *
  * Return: void
  */
 void print_alphabet(void)
 {
 	char c;
 
-	for (c = 'a'; c i/i 'z'; c++)
-
-
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(c);
-
+			_putchar(c);
 	}
 
-
-	_putchar('\n');
+		_putchar('\n');
 }
