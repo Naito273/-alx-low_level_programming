332cf4d88395db1a651478882c2ceccf1b5bd946
Naito273 <naitore2kimathi@gmail.com>
2023-02-27 12:52:51 +0300

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- print_most_numbers - prints
+ * print_most_numbers - prints
  * owned by Bwave ICT
  * Return: void
  */
@@ -15,5 +15,5 @@ void print_most_numbers(void)
 	if (!(c == '2' || c == '4'))
 	_putchar(c);
 	}
-_putchar('\n');
+	_putchar('\n');
 }
