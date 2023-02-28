34db93d10bffa01ea4ed092e63f13c83caa3ba7f
Naito273 <naitore2kimathi@gmail.com>
2023-02-28 11:52:37 +0300

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * more_numbers - nmbr fct
- * Bwave ICT file
  * Return: no return
  */
 
@@ -14,13 +13,13 @@ void more_numbers(void)
 	for (i = 1; i <= 10; i++)
 				{
 	for (j = 0; j <= 14; j++)
+
+	if (j >= 10)
 	{
+	_putchar('1');
+	_putchar(j % 10 + '0');
+																}
 
-		if (j >= 10)
-
-		_putchar('1');
-
-		_putchar(j % 10 + '0');
-	}
 		_putchar('\n');
 	}
+}
